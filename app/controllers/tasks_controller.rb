51bb1b386d9f87602f9a752fc586c6f52e267531
kanadewatanabe <kanadearo_3103@yahoo.co.jp>
2017-06-14 17:58:34 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,19 +1,23 @@
 class TasksController < ApplicationController
+before_action :require_user_logged_in, only: [:show, :new, :create, :edit, :update, :destroy]
 before_action :set_task, only: [:show, :edit, :update, :destroy]
+before_action :correct_user, only: [:destroy]
 
   def index
-    @tasks = Task.all.page(params[:page])
+    if logged_in?
+      @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
+    end  
   end
 
   def show
   end
 
   def new
-    @task = Task.new
+    @task = current_user.tasks.build
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
       
     if @task.save
       flash[:success] = 'Taskが正常に投稿されました。'
@@ -47,11 +51,18 @@ before_action :set_task, only: [:show, :edit, :update, :destroy]
 private
 
 def set_task
-  @task = Task.find(params[:id])
+  @task = current_user.tasks.find(params[:id])
 end  
 
 def task_params
   params.require(:task).permit(:content, :title)
 end  
 
+def correct_user 
+  @task = current_user.tasks.find_by(id: params[:id])
+  unless @task
+    redirect_to tasks_url
+  end
+end  
+  
 end
